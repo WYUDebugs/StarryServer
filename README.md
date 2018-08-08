304837b024a08dcae8c961c233efade6082e57b5
wunaifu <1278022159@qq.com>
2018-08-08 09:06:19 +0800

    starry后台服务器项目初版

---
+++ README.md
@@ -0,0 +1,123 @@
+# SSM-Maven
+
+### idea激活
+去 http://idea.lanyus.com/ 获取验证码，复制
+
+![](images/微信截图_20180803150359.png)
+
+关闭软件，修改host文件 C:\Windows\System32\drivers\etc
+在host中加入 0.0.0.0 account.jetbrains.com
+激活成功
+
+* 基于maven的spring、springMVC、Mybatis项目
+* 推荐url接口测试软件：postman.exe
+![](images/微信截图_20180728143358.png)
+* 推荐插件：方便mapper文件间跳转及sql语句编写
+
+![](images/微信截图_20180728141651.png)
+
+##IntelliJ IDEA中安装、配置以及使用Maven
+链接：https://blog.csdn.net/ck4438707/article/details/53377413  （或自行百度学习）
+
+## Maven项目导入，按照图片，next到finish，项目导入成功
+（项目导入成功后检查下项目中IDEA的Maven仓库路径）
+
+![](images/微信截图_20180728113345.png)
+
+![](images/微信截图_20180728113553.png)
+
+![](images/微信截图_20180728113705.png)
+
+![](images/微信截图_20180728113722.png)
+
+![](images/微信截图_20180728113741.png)
+
+![](images/微信截图_20180728113805.png)
+
+（项目导入成功后检查下项目中IDEA的Maven仓库路径）
+### 配置Tomcat
+
+![](images/微信截图_20180728141046.png)
+
+![](images/微信截图_20180728141117.png)
+
+![](images/微信截图_20180728141150.png)
+
+![](images/微信截图_20180728141253.png)
+
+![](images/微信截图_20180728141317.png)
+
+
+## 编码
+### 请求时数据走向
+* 1、URL请求
+* 2、controller处理请求，获取请求参数，调用service接口，传参数到业务层。
+controller一般只获取请求参数，返回结果，不进行复杂的数据操作，复杂的数据操作交给业务层
+* 3、service调用dao层接口，读写数据库数据，进行复杂的数据处理，返回结果给controller层
+* 4、dao层读写数据库数据，返回读写结果
+
+### 例：
+请求链接：http://localhost:8080/user/findUserByPhone?phone=1
+访问后数据请求走向如下图，数据处理后controller返回处理结果（处理成json数据）
+![](images/微信截图_20180728120121.png)
+
+![](images/微信截图_20180728134902.png)
+
+### 文件
+* main：后台项目文件
+* test：测试main里面dao和service里面的方法，放置测试类
+* entity:实体类
+* dao：数据访问对象的类即dao层类
+* service：业务层类
+* controller：控制层类
+* dto:传输类，例如约影功能需要的实体类，里面定义的变量有对应user_tab表的用户的姓名，性别等和约影表中的电影名，地名，约影详情等。
+* utils：工具包，如时间格式的转换工具类TimeUtil
+* resources：一些配置的资源文件
+* mapper：对应dao包中的类，实现访问数据库操作的SQL语句
+* spring：mybatis，spring，springMVC的整合配置文件
+* jdbc.properties:连接数据库文件
+* mybatis-config.xml:mybatis配置文件
+* Pom.xml;POM包括了所有的项目信息，jar包的依赖等
+    groupId:项目或者组织的唯一标志，并且配置时生成路径也是由此生成，如org.myproject.mojo生成的相对路径为：/org/myproject/mojo
+    artifactId:项目的通用名称
+    version:项目的版本
+
+### springMVC及Mybatis原理图
+
+![](images/SpringMVC结构图.png)
+![](images/myBatis原理图.jpg)
+
+## 学习要点
+#### 1、Java基础
+
+Java基本语法、面向对象相关的基本概念与思想，常用String类的api，
+异常处理，IO基础，容器，多线程，内存管理与垃圾回收， 
+知道几种常见的 Java 设计模式等，建议可以找些网上Java面试宝典之类的文章，
+熟悉下面试常遇到的一些Java知识点，一般都是Java基础。
+
+#### 2、框架基础
+
+spring、springMVC、mybatis、hibernate（springboot）
+能使用以下技术搭建项目
+* springMVC + spring + mybatis
+* springMVC + spring + hibernate
+* springboot + mybatis
+
+### 了解以上框架原理
+其次还需要掌握HTML、CSS、JQurey、ajax、bootstrap、MySQL、
+http、socket、json、restful
+
+#### 3、Android基础
+* Android基础UI控件。Button、TextView、EditText、CheckBox、RadioButton、
+ImageView、Spinner、ProgressBar、SeekBar、RecycleView、ScrollView、
+LinearLayout、RelativeLayout等
+* Android四大组件的理解与熟练掌握。
+Activity的四种启动模式，Fragment的生命周期、Fragment与Activity之间的关系，BroadcastReceiver、ContentProvider、Service的使用场景与具体用法，更细节点的如 BroadcastReceiver 的广播类型与不同的注册方式的区别等都应该关注并理解到位。
+* 数据存储。Sqlite与SQL语句得掌握吧，数据库虽然说在客户端开发上只有特定的业务或者场景才用得到，
+但是SQL语句这是基础，基本的操作sqlite相关的api也必须要掌握。
+* 了解常见的数据格式与解析方法，虽然目前常用的数据格式就是json，解析库也有很多，如Gson等。
+
+
+
+
+
---
+++ starry_db.sql
@@ -0,0 +1,274 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.51 : Database - starry_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`starry_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `starry_db`;
+
+/*Table structure for table `friend_tab` */
+
+DROP TABLE IF EXISTS `friend_tab`;
+
+CREATE TABLE `friend_tab` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '好友表id',
+  `user_id` int(11) NOT NULL,
+  `friend_id` int(11) NOT NULL,
+  `friend_name` varchar(15) DEFAULT NULL,
+  `add_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `user_id` (`user_id`),
+  KEY `friend_id` (`friend_id`),
+  CONSTRAINT `friend_tab_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_tab` (`id`),
+  CONSTRAINT `friend_tab_ibfk_2` FOREIGN KEY (`friend_id`) REFERENCES `user_tab` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `friend_tab` */
+
+/*Table structure for table `interaction_tab` */
+
+DROP TABLE IF EXISTS `interaction_tab`;
+
+CREATE TABLE `interaction_tab` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '互动记录表id',
+  `my_id` int(11) NOT NULL COMMENT '我的id',
+  `his_id` int(11) NOT NULL COMMENT '好友id',
+  `first_add_time` datetime DEFAULT NULL COMMENT '加好友时间',
+  `first_chat_time` datetime DEFAULT NULL,
+  `first_good_time` datetime DEFAULT NULL,
+  `first_comment_time` datetime DEFAULT NULL,
+  `lately_chat_time` datetime DEFAULT NULL,
+  `lately_good_time` datetime DEFAULT NULL,
+  `lately_comment_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `interaction_tab` */
+
+/*Table structure for table `memory_book_tab` */
+
+DROP TABLE IF EXISTS `memory_book_tab`;
+
+CREATE TABLE `memory_book_tab` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '纪念册表id',
+  `owner` int(11) NOT NULL COMMENT '纪念册创建者id',
+  `title` varchar(15) DEFAULT NULL COMMENT '标题',
+  `cover` varchar(100) DEFAULT NULL COMMENT '封面',
+  `creat_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `friend_count` int(11) DEFAULT '0' COMMENT '共同编辑好友数',
+  `moment_count` int(11) DEFAULT '0' COMMENT '片段数目',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `owner` (`owner`),
+  CONSTRAINT `memory_book_tab_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `user_tab` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `memory_book_tab` */
+
+/*Table structure for table `memory_cover_tab` */
+
+DROP TABLE IF EXISTS `memory_cover_tab`;
+
+CREATE TABLE `memory_cover_tab` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '纪念册默认封面表id',
+  `path` varchar(100) DEFAULT NULL COMMENT '默认封面路径',
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `memory_cover_tab` */
+
+/*Table structure for table `memory_friend_tab` */
+
+DROP TABLE IF EXISTS `memory_friend_tab`;
+
+CREATE TABLE `memory_friend_tab` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '纪念册好友编辑表id',
+  `friend_id` int(11) NOT NULL COMMENT '好友id',
+  `memory_book_id` int(11) NOT NULL COMMENT '纪念册id',
+  `add_time` datetime DEFAULT NULL COMMENT '添加时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `friend_id` (`friend_id`),
+  KEY `memory_book_id` (`memory_book_id`),
+  CONSTRAINT `memory_friend_tab_ibfk_1` FOREIGN KEY (`friend_id`) REFERENCES `user_tab` (`id`),
+  CONSTRAINT `memory_friend_tab_ibfk_2` FOREIGN KEY (`memory_book_id`) REFERENCES `memory_book_tab` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `memory_friend_tab` */
+
+/*Table structure for table `moment_comment_tab` */
+
+DROP TABLE IF EXISTS `moment_comment_tab`;
+
+CREATE TABLE `moment_comment_tab` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '片段评论表id',
+  `commentator` int(11) NOT NULL COMMENT '评论者id',
+  `moment_id` int(11) NOT NULL COMMENT '片段表id',
+  `comment_time` datetime DEFAULT NULL COMMENT '评论时间',
+  `comment_content` text COMMENT '评论内容',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `commentator` (`commentator`),
+  KEY `moment_id` (`moment_id`),
+  CONSTRAINT `moment_comment_tab_ibfk_2` FOREIGN KEY (`moment_id`) REFERENCES `moment_tab` (`id`),
+  CONSTRAINT `moment_comment_tab_ibfk_1` FOREIGN KEY (`commentator`) REFERENCES `user_tab` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `moment_comment_tab` */
+
+/*Table structure for table `moment_image_tab` */
+
+DROP TABLE IF EXISTS `moment_image_tab`;
+
+CREATE TABLE `moment_image_tab` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '片段图片表id',
+  `moment_id` int(11) NOT NULL COMMENT '片段表id',
+  `path` varchar(100) DEFAULT NULL COMMENT '图片路径',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `moment_id` (`moment_id`),
+  CONSTRAINT `moment_image_tab_ibfk_1` FOREIGN KEY (`moment_id`) REFERENCES `moment_tab` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `moment_image_tab` */
+
+/*Table structure for table `moment_tab` */
+
+DROP TABLE IF EXISTS `moment_tab`;
+
+CREATE TABLE `moment_tab` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '纪念册片段表id',
+  `sender` int(11) NOT NULL COMMENT '发布者id',
+  `send_time` datetime DEFAULT NULL COMMENT '发布时间',
+  `location_time` datetime DEFAULT NULL COMMENT '事件定位时间',
+  `content` text COMMENT '内容',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `sender` (`sender`),
+  CONSTRAINT `moment_tab_ibfk_1` FOREIGN KEY (`sender`) REFERENCES `user_tab` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `moment_tab` */
+
+/*Table structure for table `public_comment_tab` */
+
+DROP TABLE IF EXISTS `public_comment_tab`;
+
+CREATE TABLE `public_comment_tab` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '动态评论表id',
+  `commentator` int(11) NOT NULL COMMENT '评论者id',
+  `public_id` int(11) NOT NULL COMMENT '动态表id',
+  `comment_time` datetime DEFAULT NULL COMMENT '评论时间',
+  `comment_content` text COMMENT '评论内容',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `commentator` (`commentator`),
+  KEY `public_id` (`public_id`),
+  CONSTRAINT `public_comment_tab_ibfk_1` FOREIGN KEY (`commentator`) REFERENCES `user_tab` (`id`),
+  CONSTRAINT `public_comment_tab_ibfk_2` FOREIGN KEY (`public_id`) REFERENCES `public_tab` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `public_comment_tab` */
+
+/*Table structure for table `public_concern_tab` */
+
+DROP TABLE IF EXISTS `public_concern_tab`;
+
+CREATE TABLE `public_concern_tab` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '动态关注表id',
+  `concern_people` int(11) NOT NULL COMMENT '关注收藏着id',
+  `concern_public` int(11) NOT NULL COMMENT '动态表id',
+  `concern_time` datetime DEFAULT NULL COMMENT '收藏时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `concern_people` (`concern_people`),
+  KEY `concern_public` (`concern_public`),
+  CONSTRAINT `public_concern_tab_ibfk_1` FOREIGN KEY (`concern_people`) REFERENCES `user_tab` (`id`),
+  CONSTRAINT `public_concern_tab_ibfk_2` FOREIGN KEY (`concern_public`) REFERENCES `public_tab` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `public_concern_tab` */
+
+/*Table structure for table `public_good_tab` */
+
+DROP TABLE IF EXISTS `public_good_tab`;
+
+CREATE TABLE `public_good_tab` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '动态点赞表id',
+  `man_of_praise` int(11) NOT NULL,
+  `time_of_praise` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `man_of_praise` (`man_of_praise`),
+  CONSTRAINT `public_good_tab_ibfk_1` FOREIGN KEY (`man_of_praise`) REFERENCES `user_tab` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `public_good_tab` */
+
+/*Table structure for table `public_image_tab` */
+
+DROP TABLE IF EXISTS `public_image_tab`;
+
+CREATE TABLE `public_image_tab` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '动态图片列表id',
+  `public_id` int(11) NOT NULL COMMENT '动态表id',
+  `path` varchar(100) DEFAULT NULL COMMENT '图片路径',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `public_id` (`public_id`),
+  CONSTRAINT `public_image_tab_ibfk_1` FOREIGN KEY (`public_id`) REFERENCES `public_tab` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `public_image_tab` */
+
+/*Table structure for table `public_tab` */
+
+DROP TABLE IF EXISTS `public_tab`;
+
+CREATE TABLE `public_tab` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '动态表id',
+  `publisher` int(11) NOT NULL COMMENT '动态发布者id',
+  `time` datetime DEFAULT NULL COMMENT '发布时间',
+  `content` text COMMENT '内容',
+  `times_of_browse` int(11) DEFAULT NULL COMMENT '浏览数',
+  `times_of_praise` int(11) DEFAULT NULL COMMENT '点赞数',
+  `number_of_comments` int(11) DEFAULT NULL COMMENT '评论数',
+  `collection_number` int(11) DEFAULT NULL COMMENT '关注收藏数',
+  `address` varchar(100) DEFAULT NULL COMMENT '定位地址',
+  `type` int(11) DEFAULT '0' COMMENT '动态类型，默认为0发布的动态，1分享过来的动态',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `publisher` (`publisher`),
+  CONSTRAINT `public_tab_ibfk_1` FOREIGN KEY (`publisher`) REFERENCES `user_tab` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `public_tab` */
+
+/*Table structure for table `user_tab` */
+
+DROP TABLE IF EXISTS `user_tab`;
+
+CREATE TABLE `user_tab` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `phone` varchar(15) NOT NULL,
+  `password` varchar(70) NOT NULL,
+  `name` varchar(15) DEFAULT NULL,
+  `age` int(11) DEFAULT '0',
+  `gender` int(11) DEFAULT '0',
+  `birthday` date DEFAULT NULL,
+  `signature` text,
+  `headimage` varchar(100) DEFAULT NULL,
+  `typelabel` varchar(100) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `phoneId` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `phone` (`phone`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `user_tab` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
