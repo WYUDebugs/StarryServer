64101140498ad95adddc14173f8e360b35d9365b
Zq <837748078@qq.com>
2018-08-25 20:11:36 +0800

    memory_cover_tab功能的小修改

---
+++ src/test/java/com/zhuolang/starryserver/dao/MemoryCoverDaoTest.java
@@ -0,0 +1,20 @@
+package com.zhuolang.starryserver.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MemoryCoverDaoTest {
+
+    @Test
+    public void addMemoryCover() {
+    }
+
+    @Test
+    public void deleteMemoryCover() {
+    }
+
+    @Test
+    public void findAllMemoryCoverDESC() {
+    }
+}
